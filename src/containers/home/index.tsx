485627db9a4ec926f53d33ed8fb0bf1a15ca8d66
An Bui <anbui@imt-soft.com>
2024-05-10 09:05:43 +0700

    setup project structure and package

---
+++ src/containers/home/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
